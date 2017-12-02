a4c55be8d6a6476d81b675b483372e93b58e8a70
David Tulig <david.tulig@gmail.com>
2017-12-02 06:06:27 -0800

    duplicity: 0.7.12 -> 0.7.15
    Adds a parameter, defaulting to false, to build with the backblaze
    client library, which is a requirement for the backblaze backend. This
    also adds a patch to fix an issue with using gpg keys.

--- pkgs/tools/backup/duplicity/default.nix
+++ pkgs/tools/backup/duplicity/default.nix
@@ -1,19 +1,24 @@
-{ stdenv, fetchurl, python2Packages, librsync, ncftp, gnupg, rsync, makeWrapper }:
+{ stdenv, fetchurl, lib, python2Packages, librsync, ncftp, gnupg, rsync, makeWrapper
+, withBackblazeB2 ? false, backblaze-b2
+}:
 
 python2Packages.buildPythonApplication rec {
   name = "duplicity-${version}";
-  version = "0.7.12";
+  version = "0.7.15";
 
   src = fetchurl {
     url = "http://code.launchpad.net/duplicity/0.7-series/${version}/+download/${name}.tar.gz";
-    sha256 = "1rhgrz2lm9vbfdp2raykrih1c6n2lw5jd572z4dsz488m52avjqi";
+    sha256 = "0v0a8pmnaa8b634gh9kqzbr3f6971sxbjshl6sqfr11j84a7vgsh";
   };
+
+  # Can be removed in 0.7.16: https://bugs.launchpad.net/duplicity/+bug/1733057
+  patches = [ ./duplicity-collections.patch ];
 
   buildInputs = [ librsync makeWrapper python2Packages.wrapPython ];
   propagatedBuildInputs = with python2Packages; [
     boto cffi cryptography ecdsa enum idna pygobject3
-    ipaddress lockfile paramiko pyasn1 pycrypto six
-  ];
+    ipaddress fasteners paramiko pyasn1 pycrypto six
+  ] ++ lib.optionals withBackblazeB2 [ backblaze-b2 ];
   checkInputs = with python2Packages; [ lockfile mock pexpect ];
 
   # lots of tests are failing, although we get a little further now with the bits in preCheck
